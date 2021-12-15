dd896052097ba467f5ec317edede26912f95ca4c
fra <fra.sette.p.c@gmail.com>
2021-12-15 15:14:43 +0100

    installer

--- installer.sh
+++ installer.sh
@@ -10,9 +10,9 @@ if [ "$answer" == "y" ]; then
   git clone git@github.com:frr0/Covid-Today.git
 else
   git clone https://github.com/frr0/Covid-Today.git
+fi
 
 cd Covid-Today
 sudo cp program.sh /usr/local/bin
 cd /usr/local/bin
 sudo rename program.sh covidtoday program.sh
-
