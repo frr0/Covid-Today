420043b27efbf1c3a822b102d80d334d89d56a53
fra <fra.sette.p.c@gmail.com>
2021-12-15 16:43:54 +0100

    prog

--- program.sh
+++ program.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-rm dpc-covid19-ita-scheda-regioni-latest.pdf
+if [ -f dpc-covid19-ita-scheda-regioni-latest.pdf ]; then
+  rm dpc-covid19-ita-scheda-regioni-latest.pdf
+fi
 
 wget -q https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/schede-riepilogative/regioni/dpc-covid19-ita-scheda-regioni-latest.pdf
 
 xdg-open dpc-covid19-ita-scheda-regioni-latest.pdf
+
+rm dpc-covid19-ita-scheda-regioni-latest.pdf
 
 exit 0
